3b2a41c1c932d0620bb229ab3f303e52502a67b6
Aitor Jorge Jiménez <92213588+aitorJJ@users.noreply.github.com>
2021-12-04 12:14:51 +0100

    Add files via upload

---
+++ PROYECTO_ISO2/src/main/java/dominio/Empleado.java
@@ -0,0 +1,58 @@
+package dominio;
+//V 0.1.0 
+//changelog: Añadidas las funciones de la iteracion 2
+public class Empleado extends Persona {
+
+	private int ID_Empleado;
+	private int Sueldo;
+
+	/**
+	 * 
+	 * @param Enfermedad
+	 */
+	public void addEnfermedad(Enfermedad Enfermedad) {
+
+	}
+
+	public int getID_Empleado() {
+		// TODO - implement Empleado.getID_Empleado
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param ID_Empleado
+	 */
+	public void setID_Empleado(int ID_Empleado) {
+		// TODO - implement Empleado.setID_Empleado
+		throw new UnsupportedOperationException();
+	}
+
+	public int getSueldo() {
+		// TODO - implement Empleado.getSueldo
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Sueldo
+	 */
+	public void setSueldo(int Sueldo) {
+		// TODO - implement Empleado.setSueldo
+		throw new UnsupportedOperationException();
+	}
+
+	public void comprobarInventario() {
+		// TODO - implement Empleado.comprobarInventario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param prevision
+	 */
+	public void registrarPrevisiones(Prevision prevision) {
+
+	}
+
+}
---
+++ PROYECTO_ISO2/src/main/java/dominio/Enfermedad.java
@@ -0,0 +1,66 @@
+package dominio;
+//V 0.0.0
+public class Enfermedad {
+
+	private String Nombre;
+	private String Tipo;
+	private String Peligrosidad;
+	private String Sintomas;
+
+	public String getNombre() {
+		// TODO - implement Enfermedad.getNombre
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Nombre
+	 */
+	public void setNombre(String Nombre) {
+		// TODO - implement Enfermedad.setNombre
+		throw new UnsupportedOperationException();
+	}
+
+	public String getTipo() {
+		// TODO - implement Enfermedad.getTipo
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Tipo
+	 */
+	public void setTipo(String Tipo) {
+		// TODO - implement Enfermedad.setTipo
+		throw new UnsupportedOperationException();
+	}
+
+	public String getPeligrosidad() {
+		// TODO - implement Enfermedad.getPeligrosidad
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Peligrosidad
+	 */
+	public void setPeligrosidad(String Peligrosidad) {
+		// TODO - implement Enfermedad.setPeligrosidad
+		throw new UnsupportedOperationException();
+	}
+
+	public String getSintomas() {
+		// TODO - implement Enfermedad.getSintomas
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Sintomas
+	 */
+	public void setSintomas(String Sintomas) {
+		// TODO - implement Enfermedad.setSintomas
+		throw new UnsupportedOperationException();
+	}
+
+}
