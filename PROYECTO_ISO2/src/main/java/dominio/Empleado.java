065c4b8f7d37d6110a5e8b9427d21bd1f8794ac2
davidValbuenaUCLM <91552740+davidValbuenaUCLM@users.noreply.github.com>
2021-12-05 11:40:49 +0100

    Iteración 3
    Caso de uso 4

---
+++ PROYECTO_ISO2/src/main/java/dominio/Empleado.java
@@ -0,0 +1,146 @@
+package dominio;
+
+public class Empleado extends Persona {
+
+	private int ID_Empleado;
+	private int Sueldo;
+
+	/**
+	 * 
+	 * @param Enfermedad
+	 */
+	public void addEnfermedad(Enfermedad Enfermedad) {
+		// TODO - implement Empleado.addEnfermedad
+		throw new UnsupportedOperationException();
+	}
+
+	public int getID_Empleado() {
+		// TODO - implement Empleado.getID_Empleado
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param ID_Empleado
+	 */
+	public void setID_Empleado(int ID_Empleado) {
+		// TODO - implement Empleado.setID_Empleado
+		throw new UnsupportedOperationException();
+	}
+
+	public int getSueldo() {
+		// TODO - implement Empleado.getSueldo
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Sueldo
+	 */
+	public void setSueldo(int Sueldo) {
+		// TODO - implement Empleado.setSueldo
+		throw new UnsupportedOperationException();
+	}
+
+	public void comprobarInventario() {
+		// TODO - implement Empleado.comprobarInventario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param prevision
+	 */
+	public void registrarPrevisiones(Prevision prevision) {
+		// TODO - implement Empleado.registrarPrevisiones
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Paciente
+	 */
+	public void getInformacionPaciente(Paciente Paciente) {
+		// TODO - implement Empleado.getInformacionPaciente
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Paciente
+	 */
+	public void addPaciente(Paciente Paciente) {
+		// TODO - implement Empleado.addPaciente
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Paciente
+	 */
+	public void solicitarMensajePaciente(Paciente Paciente) {
+		// TODO - implement Empleado.solicitarMensajePaciente
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param ID_Paciente
+	 */
+	public Paciente getInformacionPaciente(int ID_Paciente) {
+
+	}
+
+	/**
+	 * 
+	 * @param Paciente
+	 */
+	public void addPaciente(Paciente Paciente) {
+		// TODO - implement Empleado.addPaciente
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Paciente
+	 */
+	public boolean solicitarMensajePaciente(Paciente Paciente) {
+
+	}
+
+	/**
+	 * 
+	 * @param Paciente
+	 */
+	public void addPaciente(Paciente Paciente) {
+		// TODO - implement Empleado.addPaciente
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Paciente
+	 */
+	public boolean solicitarSMSPaciente(Paciente Paciente) {
+
+	}
+
+	/**
+	 * 
+	 * @param Paciente
+	 */
+	public void addPaciente(Paciente Paciente) {
+		// TODO - implement Empleado.addPaciente
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Paciente
+	 */
+	public boolean solicitarSMSPaciente(Paciente Paciente) {
+		// TODO - implement Empleado.solicitarSMSPaciente
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ PROYECTO_ISO2/src/main/java/dominio/SESCAM.java
@@ -0,0 +1,59 @@
+package dominio;
+
+public class SESCAM {
+
+	private string NIF;
+	private string Sede;
+
+	public string getNIF() {
+		// TODO - implement SESCAM.getNIF
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param NIF
+	 */
+	public void setNIF(string NIF) {
+		// TODO - implement SESCAM.setNIF
+		throw new UnsupportedOperationException();
+	}
+
+	public string getSede() {
+		// TODO - implement SESCAM.getSede
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Sede
+	 */
+	public void setSede(string Sede) {
+		// TODO - implement SESCAM.setSede
+		throw new UnsupportedOperationException();
+	}
+
+	public void comprobarInventario() {
+		// TODO - implement SESCAM.comprobarInventario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Prevision
+	 */
+	public void registrarPrevisiones(Prevision Prevision) {
+		// TODO - implement SESCAM.registrarPrevisiones
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Paciente
+	 */
+	public boolean enviarSMS(Paciente Paciente) {
+		// TODO - implement SESCAM.enviarMensaje
+		throw new UnsupportedOperationException();
+	}
+
+}
